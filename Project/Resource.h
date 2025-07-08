6a10fd0cc15b7499acd02b80a5e5f9ede47a84dc
MartEssence <martessence.shop@gmail.com>
2025-07-08 13:46:34 +0530

    Add files via upload

---
+++ Project/Resource.h
@@ -0,0 +1,94 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ValVoice.rc
+
+#pragma once
+
+// --------------------
+// Main Dialog & Tabs
+// --------------------
+#define IDD_VALVOICE_DIALOG          100
+#define IDC_TAB_MAIN                 101
+#define IDC_STATIC      -1
+
+#define IDD_TAB_MAIN                 110
+#define IDD_TAB_USER                 120
+#define IDD_TAB_INFO                 130
+#define IDD_TAB_SETTINGS             140
+
+// --------------------
+// Main Tab Controls
+// --------------------
+#define IDC_TEXT_INPUT               1001
+#define IDC_SPEAK_BUTTON             1002
+#define IDC_STOP_BUTTON              1003
+#define IDC_RATE_SLIDER              1004
+#define IDC_VOLUME_SLIDER            1005
+#define IDC_AGENT_COMBO              1006
+#define IDC_VOICE_COMBO              1007
+#define IDC_QUOTA_BAR                1008
+#define IDC_QUOTA_VALUE              1009
+#define IDC_STATS_MSGS               1010
+#define IDC_STATS_CHARS              1011
+
+// --------------------
+// User Tab Controls
+// --------------------
+#define IDC_USER_ID                  1101
+#define IDC_PREMIUM_STATUS           1102
+#define IDC_CHANGE_USER              1103
+#define IDC_SOURCE_COMBO             1104
+#define IDC_MIC_STREAM               1105
+#define IDC_PTT_KEY_DISPLAY          1106
+#define IDC_SET_PTT_KEY              1107
+
+// --------------------
+// Info Tab Controls
+// --------------------
+#define IDC_INFO_TEXT                1201
+#define IDC_PROFILE_PIC              2101
+#define IDC_INFO_USERID              2102
+#define IDC_INFO_QUOTA               2103
+#define IDC_INFO_PREMIUM             2104
+#define IDC_INFO_PREMIUM_BTN         2105
+#define IDC_INFO_DISCORD_BTN         2106
+
+// --------------------
+// Settings Tab Controls
+// --------------------
+#define IDC_BLOCK_INPUT              1301
+#define IDC_BLOCK_ADD                1302
+#define IDC_BLOCK_REMOVE             1303
+#define IDC_BLOCK_LIST               1304
+#define IDC_SYNC_SETTINGS            1305
+
+// --------------------
+// Login Dialog Controls
+// --------------------
+#define IDD_LOGIN                    1400
+#define IDC_LOGIN_USERID            1401
+#define IDC_LOGIN_PREMIUM           1402
+
+// --------------------
+// About Box
+// --------------------
+#define IDD_ABOUTBOX                 1500
+
+// --------------------
+// Icons & Menu
+// --------------------
+#define IDI_VALVOICE                 1600
+#define IDI_SMALL                    1601
+#define IDC_VALVOICE                 1602
+#define IDI_USER_ICON                101
+
+// --------------------
+// String Table
+// --------------------
+#define IDS_APP_TITLE                1700
+
+// --------------------
+// Menu Commands
+// --------------------
+#define IDM_ABOUT                    1800
+#define IDM_EXIT                     1801
